17a5f1dfcad7c888f49a02cc7f987dca86ba0abe
KathyHong <1151032304@qq.com>
2019-08-17 13:37:03 +0800

    finished

---
+++ src/pages/city/City.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <City-header></City-header>
+    <City-search :cities="cities"></City-search>
+    <City-list
+    :cities="cities"
+    :hot="hotCities"
+    :letter="letter"
+    ></City-list>
+    <City-alphabet :cities="cities" @change="handleLetterChange"></City-alphabet>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import CityHeader from './components/Header'
+import CitySearch from './components/Search'
+import CityList from './components/List'
+import CityAlphabet from './components/Alphabet'
+export default {
+  name: 'city',
+  components: {
+    CityHeader,
+    CitySearch,
+    CityList,
+    CityAlphabet
+  },
+  data () {
+    return {
+      cities: {},
+      hotCities: [],
+      letter: ''
+    }
+  },
+  methods: {
+    getCityInfo () {
+      axios.get('/api/city.json')
+        .then(this.handleGetCityInfoSucc)
+    },
+    handleGetCityInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.cities = data.cities
+        this.hotCities = data.hotCities
+      }
+    },
+    handleLetterChange (letter) {
+      this.letter = letter
+    }
+  },
+  mounted () {
+    this.getCityInfo()
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ src/pages/home/home.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <HomeHeader></HomeHeader>
+    <HomeSwiper :list="swiperList"></HomeSwiper>
+    <HomeIcons :list="iconList"></HomeIcons>
+    <HomeRecommand :list="recommandList"></HomeRecommand>
+    <HomeWeekend :list="weekendList"></HomeWeekend>
+  </div>
+</template>
+
+<script>
+import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
+import HomeRecommand from './components/recommand'
+import HomeWeekend from './components/weekend'
+import axios from 'axios'
+import { mapState } from 'vuex'
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader,
+    HomeSwiper,
+    HomeIcons,
+    HomeRecommand,
+    HomeWeekend
+  },
+  data () {
+    return {
+      swiperList: [],
+      iconList: [],
+      recommandList: [],
+      weekendList: [],
+      lastCity: ''
+    }
+  },
+  computed: {
+    ...mapState(['city'])
+  },
+  methods: {
+    getHomeInfo () {
+      axios.get('/api/index.json?city=' + this.city)
+        .then(this.getHomeInfoSucc)
+    },
+    getHomeInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.swiperList = data.swiperList
+        this.iconList = data.iconList
+        this.recommandList = data.recommendList
+        this.weekendList = data.weekendList
+      }
+    }
+  },
+  mounted () {
+    this.lastCity = this.city
+    this.getHomeInfo()
+  },
+  activated () {
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
+  }
+}
+</script>
+
+<style>
+  .home {
+    font-size: 50px
+  }
+</style>
